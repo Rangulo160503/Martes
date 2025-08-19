4fc7f2d295c7b1d497343b70e793159f3b7715ef
Ronald Angulo <105762511+Rangulo160503@users.noreply.github.com>
2025-08-19 07:57:40 -0600

    dd

--- CEGADB/dbo/Tables/IncapacidadEmpleado.sql
+++ CEGADB/dbo/Tables/IncapacidadEmpleado.sql
@@ -1,10 +1,14 @@
 ﻿CREATE TABLE [dbo].[IncapacidadEmpleado] (
-    [Id]                INT            IDENTITY (1, 1) NOT NULL,
-    [UsuarioID]         NVARCHAR (MAX) NOT NULL,
-    [Descripcion]       NVARCHAR (MAX) NOT NULL,
-    [ArchivoRuta]       NVARCHAR (MAX) NOT NULL,
-    [FechaPresentacion] DATETIME2 (7)  NOT NULL,
-    [Estado]            NVARCHAR (MAX) NOT NULL,
+    [Id]                INT             IDENTITY (1, 1) NOT NULL,
+    [UsuarioID]         NVARCHAR (MAX)  NOT NULL,
+    [Descripcion]       NVARCHAR (MAX)  NOT NULL,
+    [ArchivoRuta]       NVARCHAR (MAX)  NOT NULL,
+    [FechaPresentacion] DATETIME2 (7)   NOT NULL,
+    [Estado]            NVARCHAR (MAX)  NOT NULL,
+    [ArchivoContenido]  VARBINARY (MAX) NULL,
+    [ArchivoNombre]     NVARCHAR (260)  NULL,
+    [ArchivoTipo]       NVARCHAR (100)  NULL,
+    [ArchivoTamano]     BIGINT          NULL,
     CONSTRAINT [PK_IncapacidadEmpleado] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
