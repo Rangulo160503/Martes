45e2b4360f8904c0dfda72f23fdc59cd7b4eab7a
rangulo <rangulo@CR9-ITS-RANGULO>
2025-08-30 19:02:45 -0600

    Intento1

--- CEGA/obj/Debug/net8.0/CEGA.AssemblyInfo.cs
+++ CEGA/obj/Debug/net8.0/CEGA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CEGA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cda4c1aa0bb8bae9614d5dadf76ec1d66bccb22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56c7771632d0726ecd5384410b90de434dc7ea43")]
 [assembly: System.Reflection.AssemblyProductAttribute("CEGA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CEGA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
