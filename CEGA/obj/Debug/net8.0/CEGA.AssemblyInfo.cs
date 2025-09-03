359edf1c9aa2fe274cd6c620ce9a90b37ca2cae3
Ronald Angulo <105762511+Rangulo160503@users.noreply.github.com>
2025-09-03 15:46:35 -0600

    Avance

--- CEGA/obj/Debug/net8.0/CEGA.AssemblyInfo.cs
+++ CEGA/obj/Debug/net8.0/CEGA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CEGA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79402a53ab133e7ef0a7d71b9973d5c15b80ed64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62a840cd40fafb73a420de961c206c1fc1ac05c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CEGA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CEGA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
